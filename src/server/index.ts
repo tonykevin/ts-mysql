9faf08d0c1bf8cb7353b3ae872a0f5234fc2be79
tonykevin <hi@tonykevin.com>
2019-05-01 00:00:40 -0500

    display public folder

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,4 +1,5 @@
 import express = require('express')
+import path = require('path')
 
 export default class Server {
   public app: express.Application;
@@ -13,7 +14,14 @@ export default class Server {
     return new Server(port)
   }
 
+  private publicFolder() {
+    const publicPath = path.resolve(__dirname, '../public')
+
+    this.app.use(express.static(publicPath))
+  }
+
   start (cb: Function) {
-     this.app.listen(this.port, cb)
+    this.app.listen(this.port, cb)
+    this.publicFolder()
   }
 }
