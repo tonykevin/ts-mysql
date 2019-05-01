f8dbf763c008c96e661bd5a9afc8ed060541881e
tonykevin <hi@tonykevin.com>
2019-04-30 23:38:42 -0500

    configure express in Typescript

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-console.log('Typescript')
+import Server from './server'
+
+const server = Server.init(3000)
+server.start(() => {
+  console.log('server running in port 300')
+})
---
+++ src/server/index.ts
@@ -0,0 +1,19 @@
+import express = require('express')
+
+export default class Server {
+  public app: express.Application;
+  public port: number;
+
+  constructor (port: number) {
+    this.port = port
+    this.app = express()
+  }
+
+  static init (port: number) {
+    return new Server(port)
+  }
+
+  start (cb: Function) {
+     this.app.listen(this.port, cb)
+  }
+}
